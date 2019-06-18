0708806c76438e4cb28b832070ddd89052bf726d
dennis wang <dwang0816@gmail.com>
2019-06-18 17:57:01 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,13 @@
 require 'pry'
 def get_first_name_of_season_winner(data, season)
   # code here
+  binding.pry
+  
+    data[season].each do |people|
+      people.each do |k,v|
+        if v == "Winner"
+          full_name = people["name"]
+          return full_name.split(' ').first 
 end
 
 def get_contestant_name(data, occupation)

26aae17ba58c88448df8bfe493bd939d581e1d16
dennis wang <dwang0816@gmail.com>
2019-06-18 18:01:57 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -7,15 +7,29 @@ def get_first_name_of_season_winner(data, season)
       people.each do |k,v|
         if v == "Winner"
           full_name = people["name"]
-          return full_name.split(' ').first 
+          return full_name.split(' ').first
+          #split string and returns first
+        end
+      end
+    end
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |season, array|
+    array.each do |people|
+      people.each do |k,v|
+        if v == occupation
+          return people["name"]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  
 end
 
 def get_occupation(data, hometown)
